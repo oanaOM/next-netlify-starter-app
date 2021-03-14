3a7c6e60e64d85539c409d60f2c187fed6a5da6f
Oana Munteanu <hello2oana@yahoo.com>
2021-03-14 12:50:41 +0000

    add MSW servers

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function Home(){
-    return <div>Hello Explorer</div>
-}
+import { useEffect } from 'react'
 
-export default Home
+export default function Home({ book }) {
+
+  useEffect(()=>{
+      // Client-side request are mocked by `mocks/browser.js`.
+      fetch('/reviews')
+      .then((res) => res.json())
+      .then((res)=>console.log("Hello", res))
+  })
+  return (
+    <div>
+      Welcome to Next.JS + Netlify + MSW
+    </div>
+  )
+}
